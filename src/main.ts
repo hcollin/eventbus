1a6e2fd8d80092a2f9200e7acb0e37313fe6d16e
collihe <henrik.collin@cinia.fi>
2024-11-01 14:10:05 +0200

    Added types to export and bunped version to 0.6

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import { createEventBus } from "./eventbus.ts";
+import { EventBus, EventBusOptions, EventBusStats } from "./types/eventBus.types.ts";
 import { LOGLEVEL } from "./types/LogLevels.enum.ts";
 
-export { createEventBus, LOGLEVEL };
+export {
+    // Functions
+    createEventBus,
+
+    // Enums
+    LOGLEVEL,
+};
+
+export type { EventBus, EventBusStats, EventBusOptions };
